7e8b2c6bf88ea70c542ee62a916dba7fab0ba85a
Christopher Mendes <bos21001@byui.edu>
2021-10-10 00:59:37 -0300

    Christopher Mendes

--- src/novaes/HelloFromMarco.java
+++ src/novaes/HelloFromMarco.java
@@ -1,8 +1,10 @@
 package novaes;
 
 public class HelloFromMarco {
+
     public static void main(String[] args) {
         System.out.println("Hello, my name is Marco");
+        System.out.println("Hello, my name is Christopher");
     }
 
 }
